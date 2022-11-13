1e25ba5dad3cd5c70628ee3c9eb69c515e2ee039
Sabyrgul <47477919+Sabyrgul@users.noreply.github.com>
2022-11-14 02:23:02 +0600

    Update BankAccount.java

--- src/Homework/BankAccount.java
+++ src/Homework/BankAccount.java
@@ -24,18 +24,13 @@ public class BankAccount {
         System.out.println("Вы успешно добавили "+deposit+" сомов. Доступная сумма: "+sum);
     return sum;
     }
-    public double withDraw(int sum)  {
+    public double withDraw(int sum) throws LimitException {
         System.out.println("Введите требуемую сумму:");
         int withDraw=scanner.nextInt();
         if(sum<withDraw){
-            try {
                 throw new LimitException("Не хватает средств. Доступная сумма: ",sum);
-            } catch (LimitException e) {
-                System.out.println(e.getMessage() + e.getRemainingAmount());
-            sum-=e.getRemainingAmount();
-                System.out.println("Остаток: "+sum);
             }
-        }else
+        else
         sum-=withDraw;
         return sum;
     }
